03ff2ad86dad2255cb3cdbaf3ae2461c14af9a7c
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-11-03 19:36:15 +0100

    vintage style

--- src/7-vintage_style/main.py
+++ src/7-vintage_style/main.py
@@ -1,7 +1,20 @@
 import prettymaps as pm
 import matplotlib.pyplot as plt
 
-plot = pm.plot(query=(-33.884788, 151.200655), preset="heerhugowaard", radius=6000)
+plot = pm.plot(query=(44.846030, -0.566520), preset="heerhugowaard", radius=1600)
 
-plt.tight_layout()
-plt.savefig("src/7-vintage_style/vintage_style.png", dpi=500, bbox_inches="tight")
+text_args = dict(
+    transform=plt.gcf().transFigure, size=12, weight="bold", ha="center", font="Arial"
+)
+plt.text(x=0.5, y=0.935, s="#30DayMapChallenge", **text_args)
+plt.text(x=0.5, y=0.92, s="Vintage style - Joseph Barbier", **text_args)
+plt.text(
+    x=0.32,
+    y=0.06,
+    s="The Garonne River crossing Bordeaux, France",
+    transform=plt.gcf().transFigure,
+    size=17,
+    ha="center",
+    font="Arial",
+)
+plt.savefig("src/7-vintage_style/vintage_style.png", dpi=500)
